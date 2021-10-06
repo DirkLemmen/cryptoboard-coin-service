36f115f983573e33e0797a4f7f53757bc21d7202
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2021-10-06 09:54:01 +0200

    initialize spring

---
+++ src/main/java/com/example/cryptoboardcoinservice/CryptoboardCoinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptoboardcoinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoboardCoinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoboardCoinServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cryptoboardcoinservice/CryptoboardCoinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptoboardcoinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoboardCoinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
