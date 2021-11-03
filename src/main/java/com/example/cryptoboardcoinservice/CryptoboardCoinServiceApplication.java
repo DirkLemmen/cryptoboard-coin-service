19fa75672ddaf38af60a90eb678edcf446029611
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2021-11-03 11:41:47 +0100

    deleted unused imports and unused test

--- src/main/java/com/example/cryptoboardcoinservice/CryptoboardCoinServiceApplication.java
+++ src/main/java/com/example/cryptoboardcoinservice/CryptoboardCoinServiceApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
--- src/main/java/com/example/cryptoboardcoinservice/coins/CoinService.java
+++ src/main/java/com/example/cryptoboardcoinservice/coins/CoinService.java
@@ -1,13 +1,8 @@
 package com.example.cryptoboardcoinservice.coins;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
